1b80219b0e7ccaeb4bc244888fa9d104509be5d5
PratypartyY2K <kumarkinshuk14@gmail.com>
2021-07-14 16:56:32 +0530

    Added digital clock code

---
+++ Strings/digital-clock.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string convert_to_digital_time(int minutes){
+    //complete this function
+    int hr, min; string res;
+    hr = minutes / 60; // get the hour 
+    min = minutes % 60; // get the minutes
+    if (min < 10)
+        res = to_string(hr) + ":" + '0' + to_string(min);
+    else if(min>=10)
+        res = to_string(hr) + ":" + to_string(min);
+    return res;
+}
+
+int main() {
+    
+    int input; // 1180
+    cin >> input;
+    string result;
+    result = convert_to_digital_time(input);
+    cout << result << endl;
+
+    return 0;
+}
