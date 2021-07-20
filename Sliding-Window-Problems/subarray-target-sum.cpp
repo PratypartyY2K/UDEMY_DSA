acd26de2d4d2582bd2293e4ce577cf197649c01e
PratypartyY2K <kumarkinshuk14@gmail.com>
2021-07-20 22:11:38 +0530

    Added description and naive solution

--- Sliding-Window-Problems/subarray-target-sum.cpp
+++ Sliding-Window-Problems/subarray-target-sum.cpp
@@ -1,3 +1,39 @@
+/*
+Given an unsorted array of integers, find the number of subarrays having sum exactly equal to a given number k.
+*/
+
+// Naive solution
+/*
+// C++ program for
+// the above approach
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int arr[] = {10, 2, -2, -20, 10};
+  int k = -10;
+  int n = sizeof(arr) / sizeof(arr[0]);
+  int res = 0;
+ 
+  // Calculate all subarrays
+  for (int i = 0; i < n; i++)
+  {
+    int sum = 0;
+    for (int j = i; j < n; j++)
+    {
+      // Calculate required sum
+      sum += arr[j];
+ 
+      // Check if sum is equal to
+      // required sum
+      if (sum == k)
+        res++;
+    }
+  }
+  cout << (res) << endl;
+}
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
