186f738904952de8a71b56fd3bdf7e0666858fa7
PratypartyY2K <kumarkinshuk14@gmail.com>
2021-07-22 23:24:21 +0530

    Merge sort code

--- Sorting-and-Searching/merge-sort.cpp
+++ Sorting-and-Searching/merge-sort.cpp
@@ -2,11 +2,55 @@
 #include <vector>
 using namespace std;
 
-void merge(vector<int> &array, int s, int e) {
+void merge(vector<int> &arr, int s, int e) {
+    int i = s;
+    int m = (s+e)/2;
+    int j = m+1;
 
+    vector<int> temp;
+
+    while(i<=m and j<=e) {
+        if(arr[i]<arr[j]) {
+            temp.push_back(arr[i]);
+            i++;
+        }
+        else {
+            temp.push_back(arr[j]);
+            j++;
+        }
+    }
+
+    // if arrays get exhausted
+
+    // copy remaining elements from first array
+    while(i<=m) {
+        temp.push_back(arr[i++]);
+    }
+
+    // or copy remaining elements from second array
+    while(j<=e) {
+        temp.push_back(arr[j++]);
+    }
+
+    //copy back the elements from second array
+    int k = 0;
+    for(int idx = s;idx <= e; idx++) {
+        arr[idx] = temp[k++];
+    }
+
+    return;
+ 
 }
-void mergesort(vector<int> &array, int s, int e) {
-
+void mergesort(vector<int> &arr, int s, int e) {
+    // base case
+    if(s>=e) {
+        return;
+    }
+    //rec case
+    int mid = (s+e)/2;
+    mergesort(arr,s,mid);
+    mergesort(arr, mid+1, e);
+    return merge(arr,s,e);
 }
 
 int main() {
