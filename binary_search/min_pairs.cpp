e4586a966b4a8747fc3da9c09eb9b8e4a62851ca
PratypartyY2K <kumarkinshuk14@gmail.com>
2021-08-16 23:24:55 +0530

    Added minimum pairs code

--- binary_search/min_pairs.cpp
+++ binary_search/min_pairs.cpp
@@ -15,7 +15,7 @@ Sample Output
 #include <limits.h>
 #include <vector>
 using namespace std;
-
+/*
 void min_pair(vector<int> a1, vector<int> a2) {
     int min_diff = INT_MAX;
     pair<int,int> res;
@@ -33,6 +33,36 @@ void min_pair(vector<int> a1, vector<int> a2) {
         }
     }
     cout << "[" << res.first << "," << res.second << "]" << endl;
+}
+*/
+
+void min_pair(vector<int> a1, vector<int> a2) {
+    //Sort the array
+	sort(a2.begin(),a2.end());
+
+	int p1,p2;
+	int diff = INT_MAX;
+
+	//iteratte over 1 array and look for closes elements in the second array
+	for(int x : a1){
+		auto lb = lower_bound(a2.begin(),a2.end(),x) - a2.begin();
+
+		//left
+		if(lb>=1 and x - a2[lb-1] < diff){
+			diff = x - a2[lb-1];
+			p2 = x;
+			p1 = a2[lb-1];
+		}
+		//greater / right
+		if(lb!=a2.size() and a2[lb]-x < diff){
+			diff = a2[lb] - x;
+			p1 = x;
+			p2 = a2[lb];
+		}
+
+	}
+	cout<<"Min Pair "<<p1<<" and "<<p2 <<endl;
+	return;
 }
 
 int main() {
